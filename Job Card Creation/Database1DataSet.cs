7842e11aa42b29c0fd5e99aa392a0e617d8a95d6
Aakash Javia <aakashjavia@gmail.com>
2018-03-30 20:47:48 +0530

    Files Cleanup to Folders, Report Environment set, demo report done

---
+++ Job Card Creation/Database1DataSet.cs
@@ -0,0 +1,22 @@
+﻿namespace Job_Card_Creation
+{
+
+
+    partial class Database1DataSet
+    {
+        partial class InventoryDataTable
+        {
+        }
+
+        partial class order_statusDataTable
+        {
+        }
+    }
+}
+
+namespace Job_Card_Creation.Database1DataSetTableAdapters {
+    
+    
+    public partial class order_statusTableAdapter {
+    }
+}
---
+++ Job Card Creation/pages/InventoryReportView.cs
@@ -0,0 +1,45 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Job_Card_Creation.pages
+{
+    public partial class InventoryReportView : Form
+    {
+        public InventoryReportView()
+        {
+            InitializeComponent();
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True";
+            SqlConnection con = new SqlConnection(connectionString);
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.Connection = con;
+            cmd.CommandText = "select paper_type, paper_size, sheets, threshold from Inventory";
+            ReportDocument invReport = new ReportDocument();
+            invReport.Load(@"G:\RPNP Databases\VISUAL STUDIO\Repositories\POHSv2.0\Job Card Creation\reports\InventoryReport.rpt");
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sda.Fill(ds, "Inventory");
+            invReport.SetDataSource(ds);
+            crystalReportViewer1.ReportSource = invReport;
+            crystalReportViewer1.Refresh();
+
+            invReport.ExportToDisk(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat, @"G:\RPNP Databases\VISUAL STUDIO\Repositories\POHSv2.0\Job Card Creation\saved_reports\Inventory.pdf");
+
+            MessageBox.Show("Exported Successful");
+        }
+    }
+}
